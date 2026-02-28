aa7632a487551842997d9189750ebf8abb618b16
Jaid Lozano <jaidlozanoreyes@gmail.com>
2026-02-27 19:15:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -13,34 +13,15 @@ logoIntro.addEventListener("click", () => {
 
 });
 
-/* ACORDEÓN */
-
-function toggleCard(element){
-    const card = element.parentElement;
-    const allCards = document.querySelectorAll(".card");
-
-    allCards.forEach(c => {
-        if(c !== card){
-            c.classList.remove("active");
-        }
-    });
-
-    card.classList.toggle("active");
-}
-
-/* SCROLL SUAVE */
-
 function scrollToSection(){
     document.getElementById("paquetes").scrollIntoView({
         behavior:"smooth"
     });
 }
 
-/* WHATSAPP */
-
 function contactar(paquete){
-    const numero = "5591461227"; 
-    const mensaje = `Hola, estoy interesado en el paquete: ${paquete}`;
+    const numero = "5591461227";
+    const mensaje = `Hola Jaid, estoy interesado en el paquete: ${paquete}. Me gustaría recibir más información.`;
     const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
     window.open(url, "_blank");
 }
